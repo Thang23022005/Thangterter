e8809f19f6a91b37c865a59960bf72645f8d9463
Thang23022005 <Thangterter@gmail.com>
2025-05-14 00:27:50 +0700

    Update Main.java to demo User class

--- mavenproject1/src/main/java/com/mycompany/Practice7_5/Main.java
+++ mavenproject1/src/main/java/com/mycompany/Practice7_5/Main.java
@@ -1,11 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.Practice7_5;
 
 public class Main {
     public static void main(String[] args) {
+        // In thông báo cho Docker
         System.out.println("Hello from Docker!");
+        
+        // Demo sử dụng class User
+        User user = new User("user1", "user1@cmucs246.com");
+        System.out.println("Initial email: " + user.getEmail());
+        
+        user.setEmail("newemail@cmucs246.com");
+        System.out.println("After setEmail: " + user.getEmail());
+        
+        user.updateEmail("updatedemail@cmucs246.com");
+        System.out.println("After updateEmail: " + user.getEmail());
     }
 }
